3c2d57f4eb271966cee0971ce1f253cd381b8603
Kevin <ktroll@byu.edu>
2022-02-23 21:31:43 -0700

    Final

---
+++ Bookstore/Models/Basket.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace Bookstore.Models
+{
+    public class Basket
+    {
+        public List<BasketLineItem> Items { get; set; } = new List<BasketLineItem>();
+
+        public void AddItem (Books cate, int qty)
+        {
+            BasketLineItem line = Items
+                .Where(p => p.Books.BookId == cate.BookId)
+                .FirstOrDefault();
+
+            if (line == null)
+            {
+                Items.Add(new BasketLineItem
+                {
+                    Books = cate,
+                    Quantity = qty
+                });
+            }
+            else
+            {
+                line.Quantity += qty;
+            }
+        }
+
+
+        public double CalculateTotal()
+        {
+            double sum = Items.Sum(x => x.Quantity);
+
+            return sum;
+        }
+    }
+
+
+
+    public class BasketLineItem
+        {
+            public int LineID { get; set; }
+
+            public Books Books { get; set; }
+
+            public int Quantity { get; set; }
+        }
+    
+}
---
+++ Bookstore/Pages/Buy.cshtml.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Linq;
+using Bookstore.Infrastructure;
+using Bookstore.Models;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace Bookstore.Pages
+{
+    public class BuyModel : PageModel
+    {
+        private IBooksRepository repo { get; set; }
+
+        public BuyModel(IBooksRepository temp)
+        {
+            repo = temp;
+        }
+
+        public Basket basket { get; set; }
+
+        public string ReturnUrl { get; set; }
+
+        public void OnGet(string returnUrl)
+        {
+            ReturnUrl = returnUrl ?? "/";
+            basket = HttpContext.Session.GetJson<Basket>("basket") ?? new Basket();
+        }
+
+        public IActionResult OnPost(int bookId, string returnUrl)
+        {
+            Books p = repo.Books.FirstOrDefault(x => x.BookId == bookId);
+
+            basket = HttpContext.Session.GetJson<Basket>("basket") ?? new Basket();
+            basket.AddItem(p, 1);
+
+            HttpContext.Session.SetJeson("basket", basket);
+
+            return RedirectToPage(new { ReturnUrl = returnUrl });
+        }
+    }
+}
